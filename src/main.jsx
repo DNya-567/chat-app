e5f36708e6ef3a3ef8b0af7e510adeeeb26b2792
Dnyanesh <Dnyaneshchopade897@gmail.com>
2026-01-28 10:39:09 +0530

    versel speed package is installed with configuration

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import "./index.css";
 import App from "./App";
 
@@ -11,6 +12,7 @@ createRoot(document.getElementById("root")).render(
     <ThemeProvider>
       <AuthProvider>
         <App />
+        <SpeedInsights />
       </AuthProvider>
     </ThemeProvider>
   </StrictMode>
